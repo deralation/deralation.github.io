bd7e44190501ea4c0bbcc3817cec4b0895f36403
gokhanDerala <gderala@gmail.com>
2016-11-05 19:24:30 +0300

    bootstrap gem and fontawesome added

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	get 'about', to: 'pages#about'
+	get 'contact', to: 'pages#contact'
+	get 'home',to: 'pages#home'
+
 end
