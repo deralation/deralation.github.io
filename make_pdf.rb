9ef12ad57775f18fdbc0b1ab48edbd08c9deda00
gokhanDerala <gderala@gmail.com>
2019-03-27 23:51:01 +0300

    changes

---
+++ make_pdf.rb
@@ -0,0 +1,13 @@
+require 'pdfkit'
+
+PDFKit.configure do |config|
+	config.default_options = {
+		'disable-javascript': nil
+	}
+	config.verbose = true
+end
+
+puts "Generating PDF..."
+kit = PDFKit.new(File.new('./_site/index.html'), :page_size => 'Letter')
+file = kit.to_file('./_site/cv.pdf')
+puts "Generated PDF is here: #{file.path}"
